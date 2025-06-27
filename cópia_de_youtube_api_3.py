22f05937e58b565143e0e6fd4f92b6b786e26082
elouasb <eloahpuertas@gmail.com>
2025-06-27 09:42:27 -0300

    Update cópia_de_youtube_api_3.py

--- cópia_de_youtube_api_3.py
+++ cópia_de_youtube_api_3.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1ZBMrwgLIwp8A1Xwo5DceRG3-9y9ryCdC
 """
 
-!pip install google-api-python-client
 
 # Documentação Primária
 # https://developers.google.com/youtube/v3/docs/guideCategories?hl=pt-br
